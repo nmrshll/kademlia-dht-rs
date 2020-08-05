09aa6b4418c63abb3d8c98df5f6ab206ee6b1a74
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-08-05 19:47:08 +0100

    lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 #[macro_use]
 extern crate serde_derive;
 
+pub mod kad2;
 pub mod kademlia;
 pub mod key;
 pub mod net2;
 pub mod networking;
+pub mod rout2;
 pub mod routing;
 
 pub use kademlia::*;
