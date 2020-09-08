de73f1c437722679d1670fcdf5561ce04c6256f7
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-09-08 17:06:44 +0100

    examples: MREs for SO

---
+++ examples/so/tokio_serde_json.rs
@@ -0,0 +1,14 @@
+// use futures::prelude::*;
+// use serde_json::Value;
+// use tokio::io::{AsyncReadExt, AsyncWriteExt};
+// use tokio::net::{TcpListener, TcpStream};
+// use tokio::stream::StreamExt;
+// use tokio_serde_json::ReadJson;
+// use tokio_util::codec::{FramedRead, LengthDelimitedCodec};
+
+// pub async fn handle_stream(mut stream: TcpStream) {
+//     let length_delimited = FramedRead::new(stream, LengthDelimitedCodec::new());
+//     let mut deserialized = ReadJson::<_, Value>::new(length_delimited);
+// }
+
+fn main() {}
