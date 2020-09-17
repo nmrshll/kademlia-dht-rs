2830861550d9104dd39e211a0ee79e5a81b878d1
Nicolas Marshall <marshall.nicolas@gmail.com>
2020-09-17 12:33:28 +0100

    state manager: split kvMan and routerMan

--- src/state2.rs
+++ src/state2.rs
@@ -31,74 +31,26 @@ pub enum RouterCmd {
     // TODO maybe more ?
 }
 
-#[derive(Copy, Clone)]
-pub struct Example {
-    field: i32,
-}
-
-pub struct State {}
-impl State {
-    pub fn start(node_self: Node) -> (StateTasks, CmdChans) {
-        // Create new state
-        let mut kv: HashMap<String, String> = HashMap::new();
-        let router = RoutingTable::new(&node_self);
-
-        // A channel to send commands to the kv manager task
-        let (tx_kv, mut rx) = mpsc::channel(32);
-        let task_kv = tokio::spawn(async move {
-            while let Some(cmd) = rx.recv().await {
-                match cmd {
-                    KvCmd::Get { key, resp } => {
-                        let res = kv.get(&key).cloned();
-                        // Ignore errors
-                        let _ = resp.send(Ok(res)); // TODO error handling
-                    }
-                    KvCmd::Set { key, val, resp } => {
-                        let _res = kv.insert(key, val);
-                        // Ignore errors
-                        let _ = resp.send(Ok(())); // TODO error handling
-                    }
-                }
-            }
-        });
-
-        // A channel to send commands to the router manager task
-        let (tx_router, mut rx) = mpsc::channel(32);
-        let task_router = tokio::spawn(async move {
-            while let Some(cmd) = rx.recv().await {
-                match cmd {
-                    RouterCmd::GetClosestNodes { resp } => {
-                        let res = router.closest_nodes(Key::random(), 3); // TODO not random
-
-                        // Ignore errors
-                        let _ = resp.send(Ok(())); // TODO error handling
-                    }
-                    _ => unimplemented!(), // TODO implement other varidants
-                }
-            }
-        });
+pub struct State2 {}
+impl State2 {
+    pub fn start(node_self: Node) -> (StateTasks, StateClient) {
+        let (kv_man, task_kv) = KvStateMan::start();
+        let (router_man, task_router) = RouterStateMan::start(node_self);
 
         return (
             StateTasks {
                 kv: task_kv,
                 router: task_router,
             },
-            (tx_kv, tx_router),
+            (kv_man, router_man),
         );
     }
 }
+pub type StateClient = (KvStateMan, RouterStateMan);
 pub struct StateTasks {
     pub kv: JoinHandle<()>,
     pub router: JoinHandle<()>,
 }
-// pub struct CmdChans {
-//     kv: mpsc::Sender<KvCmd>,
-// }
-
-pub type KvCmdChan = mpsc::Sender<KvCmd>;
-pub type RouterCmdChan = mpsc::Sender<RouterCmd>;
-
-pub type CmdChans = (KvCmdChan, RouterCmdChan);
 
 #[derive(Clone)]
 pub struct KvStateMan {
@@ -152,9 +104,102 @@ impl KvStateMan {
     }
 }
 
+#[derive(Clone)]
+pub struct RouterStateMan {
+    tx: mpsc::Sender<RouterCmd>,
+}
+impl RouterStateMan {
+    pub fn start(node_self: Node) -> (Self, JoinHandle<()>) {
+        // Create state and chan to send commands to the router manager task
+        let router = RoutingTable::new(&node_self);
+        let (tx_router, mut rx) = mpsc::channel(32);
+
+        let task_router = tokio::spawn(async move {
+            while let Some(cmd) = rx.recv().await {
+                match cmd {
+                    RouterCmd::GetClosestNodes { resp } => {
+                        let res = router.closest_nodes(Key::random(), 3); // TODO not random
+
+                        // Ignore errors
+                        let _ = resp.send(Ok(())); // TODO error handling
+                    }
+                    _ => unimplemented!(), // TODO implement other varidants
+                }
+            }
+        });
+        (RouterStateMan { tx: tx_router }, task_router)
+    }
+
+    // TODO implement requests
+}
+
 use thiserror::Error;
 #[derive(Error, Debug, Clone, Serialize, Deserialize)]
 pub enum StateErr {
     #[error("unknown StateManager error")]
     Unknown,
 }
+
+/////////////////////
+/// Old state manager
+//////////
+
+pub struct State {}
+impl State {
+    pub fn start(node_self: Node) -> (StateTasks, CmdChans) {
+        // Create new state
+        let mut kv: HashMap<String, String> = HashMap::new();
+        let router = RoutingTable::new(&node_self);
+
+        // A channel to send commands to the kv manager task
+        let (tx_kv, mut rx) = mpsc::channel(32);
+        let task_kv = tokio::spawn(async move {
+            while let Some(cmd) = rx.recv().await {
+                match cmd {
+                    KvCmd::Get { key, resp } => {
+                        let res = kv.get(&key).cloned();
+                        // Ignore errors
+                        let _ = resp.send(Ok(res)); // TODO error handling
+                    }
+                    KvCmd::Set { key, val, resp } => {
+                        let _res = kv.insert(key, val);
+                        // Ignore errors
+                        let _ = resp.send(Ok(())); // TODO error handling
+                    }
+                }
+            }
+        });
+
+        // A channel to send commands to the router manager task
+        let (tx_router, mut rx) = mpsc::channel(32);
+        let task_router = tokio::spawn(async move {
+            while let Some(cmd) = rx.recv().await {
+                match cmd {
+                    RouterCmd::GetClosestNodes { resp } => {
+                        let res = router.closest_nodes(Key::random(), 3); // TODO not random
+
+                        // Ignore errors
+                        let _ = resp.send(Ok(())); // TODO error handling
+                    }
+                    _ => unimplemented!(), // TODO implement other varidants
+                }
+            }
+        });
+
+        return (
+            StateTasks {
+                kv: task_kv,
+                router: task_router,
+            },
+            (tx_kv, tx_router),
+        );
+    }
+}
+
+// pub struct CmdChans {
+//     kv: mpsc::Sender<KvCmd>,
+// }
+
+pub type KvCmdChan = mpsc::Sender<KvCmd>;
+pub type RouterCmdChan = mpsc::Sender<RouterCmd>;
+pub type CmdChans = (KvCmdChan, RouterCmdChan);
